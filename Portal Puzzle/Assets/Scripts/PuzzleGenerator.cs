29b14aeab1c656f46f7c2b333ec5b0b2b0c591e6
buitu3 <buitu3@gmail.com>
2015-09-28 11:38:26 +0700

    Update into unity5.2

--- Portal Puzzle/Assets/Scripts/PuzzleGenerator.cs
+++ Portal Puzzle/Assets/Scripts/PuzzleGenerator.cs
@@ -5,14 +5,30 @@ using System.Collections.Generic;
 public class PuzzleGenerator : MonoBehaviour {
 
     public GameObject testUnit;
+    public Sprite test;
+
+    public GameObject compareUnit;
 
     void Start()
     {
         //float x = testUnit.GetComponent<SpriteRenderer>().bounds.size.x;
         //float pixel = testUnit.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
         //float width = x * pixel;
-        StartCoroutine(shrinkUnit(this.gameObject));
-         
+        //StartCoroutine(shrinkUnit(this.gameObject));
+
+        SpriteRenderer spriteRend = testUnit.GetComponent<SpriteRenderer>();
+        spriteRend.sprite = test;   
+
+        Texture2D texture = spriteRend.sprite.texture;
+        //print(texture.height);
+        //texture.Resize(45, 45, TextureFormat.RGBA32, false);
+        //texture.Apply();
+
+        SpriteRenderer compareSpriteRend = compareUnit.GetComponent<SpriteRenderer>();
+        compareSpriteRend.sprite = Sprite.Create(texture, new Rect(0, 0, 48, 48), new Vector2(0, 0), 50.0f);
+
+        
+ 
     }
 
     IEnumerator shrinkUnit(GameObject unit)
