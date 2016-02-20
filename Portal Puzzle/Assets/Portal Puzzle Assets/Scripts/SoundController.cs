2abbdc32bda1cf777f92708b44f6c1b5c9ebf8b4
buitu3 <buitu3@gmail.com>
2016-02-20 10:38:30 +0700

    Adding Sound Effect
    Sound Effect Added :
    - Background Music added
    - Unit destroy sfx added
    - Unit moving sfx added
    UI Texture packed
    Some separate UI texture removed

---
+++ Portal Puzzle/Assets/Portal Puzzle Assets/Scripts/SoundController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+    public static SoundController Instance;
+
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	public void playSingleClip(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+        sfxSource.pitch = randomPitch;
+
+        sfxSource.PlayOneShot(clip);
+    }
+	
+}
